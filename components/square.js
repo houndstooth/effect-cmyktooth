d3c04ff2cbd529244336db9620fcfa947722be34
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-15 16:30:13 +0900

    use generic rotation code for diagonal solid square
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- components/square.js
+++ components/square.js
@@ -1,6 +1,5 @@
 import { START_ITERATION } from '../common/customize'
 import drawSolidSquare from '../../shared/render/drawSolidSquare'
-import drawSolidSquareDiagonal from '../render/drawSolidSquareDiagonal'
 import calculateStripesFunction from '../utilities/calculateStripesFunction'
 import calculateStripeColors from '../utilities/calculateStripeColors'
 import { ORIENTATION_OF_STRIPES_TO_GRID_DIAGONALITY_MAPPING } from '../common/constants'
@@ -9,8 +8,9 @@ export default ({ origin, size, orientation, squareType, iteration, color }) => 
 	if (iteration < START_ITERATION) return
 
 	if (squareType == 'SOLID_OPAQUE') {
-		const squareFunction = ORIENTATION_OF_STRIPES_TO_GRID_DIAGONALITY_MAPPING[ orientation ] ? drawSolidSquareDiagonal : drawSolidSquare
-		squareFunction({origin, size, color, scaleFromCenter: true })
+		let rotation
+		if (ORIENTATION_OF_STRIPES_TO_GRID_DIAGONALITY_MAPPING[ orientation ]) rotation = -Math.PI / 4
+		drawSolidSquare({origin, size, color, scaleFromCenter: true, rotation })
 	} else if (squareType == 'STRIPED_TOP_CUSP_OPAQUE' || squareType == 'STRIPED_TOP_CUSP_TRANSLUCENT') {
 		const stripesFunction = calculateStripesFunction(orientation)
 		const {originColor, otherColor} = calculateStripeColors({color, squareType, orientation})
