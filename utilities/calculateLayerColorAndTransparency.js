f6cc0694675dd0dba9ce14bff4359f969ce11dc1
Douglas Blumeyer <Douglas Blumeyer>
2017-05-19 22:39:16 +0900

    use names for colors

--- utilities/calculateLayerColorAndTransparency.js
+++ utilities/calculateLayerColorAndTransparency.js
@@ -1,14 +1,10 @@
 import { CMYK_MODE } from '../common/customize'
+import { CYAN, MAGENTA, YELLOW, BLACK } from '../../shared/common/constants'
 
-const CMYK_COLORS = [
-    {'r': 255, 'g': 255, 'b': 0, 'a': 1}, // yellow
-    {'r': 0, 'g': 0, 'b': 0, 'a': 1},     // black
-    {'r': 0, 'g': 255, 'b': 255, 'a': 1}, // cyan
-    {'r': 255, 'g': 0, 'b': 255, 'a': 1}  // magenta
-]
+const CMYK_COLORS = [ YELLOW, BLACK, CYAN, MAGENTA ]
 
 export default ({ iteration }) => {
-	const color = CMYK_MODE ? Object.assign({}, CMYK_COLORS[ iteration % 4 ]) : {'r': 0, 'g': 0, 'b': 0, 'a': 1}
+	const color = CMYK_MODE ? CMYK_COLORS[ iteration % 4 ] : BLACK
 	color.a = 1 / (iteration * 2)
 	return color
 }
