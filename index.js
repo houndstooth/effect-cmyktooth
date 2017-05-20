cf4317363f069f1956171f21155f8ed31bf0705c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-05-20 16:36:23 +0900

    simplify pattern entry file structure

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-import execute from './common/execute'
+import iterator from '../shared/utilities/iterator'
+import state from '../state'
+import grid from '../shared/components/grid'
 
-const cmyktooth = execute
+import calculateLayerColorAndTransparency from './utilities/calculateLayerColorAndTransparency'
+import cmyktoothTile from './components/cmyktoothTile'
 
-export { cmyktooth }
+const ORIENTATION = [
+    'TOP_RIGHT',
+    'RIGHT',
+    'BOTTOM_RIGHT',
+    'BOTTOM',
+    'BOTTOM_LEFT',
+    'LEFT',
+    'TOP_LEFT',
+    'TOP'
+]
+
+export default () => {
+    state.shared.tileSize = state.shared.canvasSize
+    iterator(state.shared.endIteration, { oneIndexed: true }).forEach(iteration => {
+        //stuff you have to do, even if not rendering this layer
+        state.cmyktooth.orientation = ORIENTATION[ iteration % 8 ]
+        state.shared.tileSize /= Math.sqrt(2)
+
+        if (iteration < state.cmyktooth.startIteration) return
+        state.cmyktooth.layerColor = calculateLayerColorAndTransparency({ iteration })
+
+        grid({ tile: cmyktoothTile })
+    })
+}
