ea95159a764d344d570dbc1dcb4a43b47405585d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-05-21 17:24:23 +0900

    separate cmyktooth and houndsmorphosis iteration counts

--- cmyktooth.js
+++ cmyktooth.js
@@ -8,7 +8,7 @@ import cmyktoothTile from './components/cmyktoothTile'
 export default () => {
     state.shared.tileSize = state.shared.canvasSize
     state.cmyktooth.layerRotation = 0
-    iterator(state.shared.endIteration, { oneIndexed: true }).forEach(iteration => {
+    iterator(state.cmyktooth.endIteration, { oneIndexed: true }).forEach(iteration => {
         state.cmyktooth.layerRotation += Math.PI / 4
         state.shared.tileSize /= Math.sqrt(2)
 
