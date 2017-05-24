9402381c899a95686f9676ad1970f1963eac13e6
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-24 17:07:58 +0900

    collapse modules imported by only one other module
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- utilities/calculateTileCenter.js
+++
@@ -1,9 +0,0 @@
-import rotateCoordinateAboutPoint from '../../shared/utilities/rotateCoordinateAboutPoint'
-import calculateCoordinateRelativeToGridCenter from './calculateCoordinateRelativeToGridCenter'
-import state from '../../shared/application/state'
-
-export default ({ origin }) => rotateCoordinateAboutPoint({
-	point: [ state.shared.canvasSize / 2, state.shared.canvasSize / 2 ],
-	coordinate: calculateCoordinateRelativeToGridCenter({ origin }),
-	rotation: state.cmyktooth.layerRotation
-})
