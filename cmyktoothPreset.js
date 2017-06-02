5c9f942f870cd49534dae1a071a8f7f2f4eb9bf2
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-02 16:42:16 +0900

    collapse shared node of state to the top
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- cmyktoothPreset.js
+++ cmyktoothPreset.js
@@ -8,36 +8,32 @@ const OFFSET_GRID = GRID_SIZE % 2 === 0 ? (GRID_SIZE / 2) % 2 : ((GRID_SIZE + 1)
 
 export default {
 	state: {
-		shared: {
-			tileSize: CMYKTOOTH_SIZE,
-			canvasSize: CMYKTOOTH_SIZE,
-			gridSize: GRID_SIZE,
-			colorConfig: {
-				set: [ CMYKTOOTH_COLORS[ (currentIteration.i + 3) % 4 ], TRANSPARENT ],
-				opacity: .5,
-				assignment: {
-					offset: [ OFFSET_GRID === 1 ? 0 : 1, OFFSET_GRID ]
-				}
-			},
-			negativeGridToo: true
+		tileSize: CMYKTOOTH_SIZE,
+		canvasSize: CMYKTOOTH_SIZE,
+		gridSize: GRID_SIZE,
+		colorConfig: {
+			set: [ CMYKTOOTH_COLORS[ (currentIteration.i + 3) % 4 ], TRANSPARENT ],
+			opacity: .5,
+			assignment: {
+				offset: [ OFFSET_GRID === 1 ? 0 : 1, OFFSET_GRID ]
+			}
 		},
+		negativeGridToo: true,
 		iteration: {
 			startIteration: 0,
 			endIteration: 16
 		}
 	},
 	iterations: {
-		shared: {
-			tileSize: p => p / Math.sqrt(2),
-			colorConfig: {
-				set: () => [ CMYKTOOTH_COLORS[ currentIteration.i % 4 ], TRANSPARENT ],
-				opacity: () => 1 / (currentIteration.i + 2)
-			},
-			gridRotationAboutCenter: p => p + (Math.PI / 4),
-			offsetOrigin: () => {
-				const offset = CMYKTOOTH_SIZE / 2 - CMYKTOOTH_SIZE / Math.pow(2, 1 + (currentIteration.i + 1) / 2)
-				return [ offset, offset ]
-			}
+		tileSize: p => p / Math.sqrt(2),
+		colorConfig: {
+			set: () => [ CMYKTOOTH_COLORS[ currentIteration.i % 4 ], TRANSPARENT ],
+			opacity: () => 1 / (currentIteration.i + 2)
+		},
+		gridRotationAboutCenter: p => p + (Math.PI / 4),
+		offsetOrigin: () => {
+			const offset = CMYKTOOTH_SIZE / 2 - CMYKTOOTH_SIZE / Math.pow(2, 1 + (currentIteration.i + 1) / 2)
+			return [ offset, offset ]
 		}
 	},
 	animations: {}
