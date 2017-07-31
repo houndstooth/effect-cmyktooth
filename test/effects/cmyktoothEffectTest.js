ec4b2164f56b784d939fd833b7a21822ab2df2ec
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-07-31 18:51:36 +0900

    another iteration, another level of abstraction in testing

--- test/effects/cmyktoothEffectTest.js
+++ test/effects/cmyktoothEffectTest.js
@@ -57,7 +57,7 @@ describe('cmyktooth effect test', () => {
         })
     })
 
-    it('iteration 1', () => {
+    it('iteration 1 is black, grain going to the right', () => {
         composeMainHoundstooth({
             houndstoothEffects: [ cmyktoothEffect ],
             houndstoothOverrides: thisIterationFrameOnly(1)
@@ -90,7 +90,7 @@ describe('cmyktooth effect test', () => {
         solid({ address: [ 3, 3 ], tileSizeInPixels, color: SEMI_BLACK, baseId: 30 })
     })
 
-    fit('iteration 2', () => {
+    it('iteration 2 is cyan, grain going to the right bottom', () => {
         composeMainHoundstooth({
             houndstoothEffects: [ cmyktoothEffect ],
             houndstoothOverrides: thisIterationFrameOnly(2)
@@ -103,7 +103,142 @@ describe('cmyktooth effect test', () => {
         const tileSizeInPixels = 800 / 4
 
         solid({ address: [ 0, 0 ], tileSizeInPixels, color: SEMI_CYAN, baseId: 0 })
-        minor({ address: [ 1, 0 ], tileSizeInPixels, colorOne: })
+        principal({ address: [ 1, 0 ], tileSizeInPixels, colors: [TRANSPARENT, SEMI_CYAN], baseId: 2 })
+        principal({ address: [ 2, 0 ], tileSizeInPixels, colors: [SEMI_CYAN, TRANSPARENT], baseId: 4 })
+        solid({ address: [ 3, 0 ], tileSizeInPixels, color: SEMI_CYAN, baseId: 6 })
+
+        principal({ address: [ 0, 1 ], tileSizeInPixels, colors: [SEMI_CYAN, TRANSPARENT], baseId: 8 })
+        solid({ address: [ 1, 1 ], tileSizeInPixels, color: TRANSPARENT, baseId: 10 })
+        solid({ address: [ 2, 1 ], tileSizeInPixels, color: TRANSPARENT, baseId: 12 })
+        principal({ address: [ 3, 1 ], tileSizeInPixels, colors: [TRANSPARENT, SEMI_CYAN], baseId: 14 })
+
+        principal({ address: [ 0, 2 ], tileSizeInPixels, colors: [TRANSPARENT, SEMI_CYAN], baseId: 16 })
+        solid({ address: [ 1, 2 ], tileSizeInPixels, color: TRANSPARENT, baseId: 18 })
+        solid({ address: [ 2, 2 ], tileSizeInPixels, color: TRANSPARENT, baseId: 20 })
+        principal({ address: [ 3, 2 ], tileSizeInPixels, colors: [SEMI_CYAN, TRANSPARENT], baseId: 22 })
+
+        solid({ address: [ 0, 3 ], tileSizeInPixels, color: SEMI_CYAN, baseId: 24 })
+        principal({ address: [ 1, 3 ], tileSizeInPixels, colors: [SEMI_CYAN, TRANSPARENT], baseId: 26 })
+        principal({ address: [ 2, 3 ], tileSizeInPixels, colors: [TRANSPARENT, SEMI_CYAN], baseId: 28 })
+        solid({ address: [ 3, 3 ], tileSizeInPixels, color: SEMI_CYAN, baseId: 30 })
+    })
+
+    fit('iteration 3 is magenta, grain going to the bottom', () => {
+        composeMainHoundstooth({
+            houndstoothEffects: [ cmyktoothEffect ],
+            houndstoothOverrides: thisIterationFrameOnly(3)
+        })
+        activateTestMarkerCanvas()
+
+        execute({ iterating: true })
+
+        const SEMI_MAGENTA = { r: 255, g: 0, b: 255, a: 0.25 }
+        const tileSizeInPixels = 800 / 8
+
+        const expectedSectorRows = [
+            [
+                ['solid', 'trans'],
+                ['minor', 'trans'],
+                ['princ', 'color'],
+                ['solid', 'color'],
+                ['solid', 'color'],
+                ['minor', 'color'],
+                ['princ', 'trans'],
+                ['solid', 'trans'],
+            ],
+            [
+                ['solid', 'trans'],
+                ['solid', 'color'],
+                ['solid', 'trans'],
+                ['solid', 'color'],
+                ['solid', 'color'],
+                ['solid', 'trans'],
+                ['solid', 'color'],
+                ['solid', 'trans'],
+            ],
+            [
+                ['princ', 'trans'],
+                ['solid', 'color'],
+                ['solid', 'trans'],
+                ['minor', 'color'],
+                ['princ', 'color'],
+                ['solid', 'trans'],
+                ['solid', 'color'],
+                ['minor', 'trans'],
+            ],
+            [
+                ['solid', 'color'],
+                ['solid', 'color'],
+                ['solid', 'trans'],
+                ['solid', 'trans'],
+                ['solid', 'trans'],
+                ['solid', 'trans'],
+                ['solid', 'color'],
+                ['solid', 'color'],
+            ],
+            [
+                ['solid', 'color'],
+                ['minor', 'color'],
+                ['princ', 'trans'],
+                ['solid', 'trans'],
+                ['solid', 'trans'],
+                ['minor', 'trans'],
+                ['princ', 'color'],
+                ['solid', 'color'],
+            ],
+            [
+                ['solid', 'color'],
+                ['solid', 'trans'],
+                ['solid', 'color'],
+                ['solid', 'trans'],
+                ['solid', 'trans'],
+                ['solid', 'color'],
+                ['solid', 'trans'],
+                ['solid', 'color'],
+            ],
+            [
+                ['princ', 'color'],
+                ['solid', 'trans'],
+                ['solid', 'color'],
+                ['minor', 'trans'],
+                ['princ', 'trans'],
+                ['solid', 'color'],
+                ['solid', 'trans'],
+                ['minor', 'color'],
+            ],
+            [
+                ['solid', 'trans'],
+                ['solid', 'trans'],
+                ['solid', 'color'],
+                ['solid', 'color'],
+                ['solid', 'color'],
+                ['solid', 'color'],
+                ['solid', 'trans'],
+                ['solid', 'trans'],
+            ],
+        ]
+
+        expectedSectorRows.forEach((expectedSectorRow, row) => {
+            expectedSectorRow.forEach((expectedSector, col) => {
+                const address = [ col, row ]
+                let method, color, colors
+                if (expectedSector[0] === 'solid') {
+                    if (expectedSector[1] === 'trans') color = TRANSPARENT
+                    if (expectedSector[1] === 'color') color = SEMI_MAGENTA
+
+                    solid({ address, tileSizeInPixels, color })
+                }
+                else {
+                    if (expectedSector[0] === 'minor') method = minor
+                    if (expectedSector[0] === 'princ') method = principal
+
+                    if (expectedSector[1] === 'trans') colors = [TRANSPARENT, SEMI_MAGENTA]
+                    if (expectedSector[1] === 'color') colors = [SEMI_MAGENTA, TRANSPARENT]
+
+                    method({ address, tileSizeInPixels, colors })
+                }
+            })
+        })
     })
 })
 
