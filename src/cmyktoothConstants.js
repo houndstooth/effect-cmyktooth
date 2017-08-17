2ba978f48589758e21ef54de6337d6943a781e2c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-08-18 07:39:48 +0900

    return cmyk to its former glory

--- src/cmyktoothConstants.js
+++ src/cmyktoothConstants.js
@@ -1,8 +1,8 @@
 import houndstoothDefaults from '../../../src/store/houndstoothDefaults'
 
-const CMYKTOOTH_GRID_SIZE = 5
+const CMYKTOOTH_GRID_SIZE = 31
 const CMYKTOOTH_CANVAS_AND_INITIAL_TILE_SIZE = houndstoothDefaults.CANVAS_SIZE
-const CMYKTOOTH_ITERATION_COUNT = 4
+const CMYKTOOTH_ITERATION_COUNT = 16
 
 export default {
 	CMYKTOOTH_GRID_SIZE,
