c33ea4ca24df2e8a99db1b4e031d9f2af904a2e0
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-09-09 18:45:24 +0900

    interface is now controls to avoid collision with reserved keyword

--- test/effects/cmyktoothEffectTest.js
+++ test/effects/cmyktoothEffectTest.js
@@ -1,6 +1,6 @@
 import cmyktoothEffect from '../../effects/cmyktoothEffect'
 import tileSectorCenterIsColor from '../../../../test/integration/helpers/tileSectorCenterIsColor'
-import executeSelectedHoundstoothEffects from '../../../../src/interface/executeSelectedHoundstoothEffects'
+import executeSelectedHoundstoothEffects from '../../../../src/controls/executeSelectedHoundstoothEffects'
 import activateTestMarkerCanvas from '../../../../test/integration/helpers/activateTestMarkerCanvas'
 import { TRANSPARENT } from '../../../../src/constants'
 import codeUtilities from '../../../../src/utilities/codeUtilities'
