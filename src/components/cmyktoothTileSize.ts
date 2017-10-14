3bb58705d2e29f2500a6e3b3aa8a9ac2f26c1c95
Douglas <douglas.blumeyer@gmail.com>
2017-10-14 23:45:05 +0800

    introduce new type, Units, nominalized, and for some reason now a lot of tests started complaining about things needing to be cast when declared, as i figured they probably would now that they are all nominally typed

--- src/components/cmyktoothTileSize.ts
+++ src/components/cmyktoothTileSize.ts
@@ -1,3 +1,5 @@
-const cmyktoothTileSize: { (p: number): number } = p => p / Math.sqrt(2)
+import { Units } from '../../../../src'
+
+const cmyktoothTileSize: { (p: Units): Units } = p => p / Math.sqrt(2)
 
 export default cmyktoothTileSize
