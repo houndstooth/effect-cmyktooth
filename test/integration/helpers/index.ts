b6e4711df2fcb5a5257aa92b95df11b3643859d2
Douglas <douglas.blumeyer@gmail.com>
2017-12-07 14:24:55 +0900

    re-export types

--- test/integration/helpers/index.ts
+++ test/integration/helpers/index.ts
@@ -1,9 +1,10 @@
 import * as sectionExpections from './sectionExpections'
 import thisLayerOnly from './thisLayerOnly'
-import { SectionExpectation } from './types'
 
 export {
-	SectionExpectation,
 	sectionExpections,
 	thisLayerOnly,
 }
+export {
+	SectionExpectation,
+} from './types'
